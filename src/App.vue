7bd9c3e80482ea3a0bc5fd80aa4c52a639305f77
HUANBIAN <1768400228@qq.com>
2019-08-16 12:00:23 +0800

    1.0

--- src/App.vue
+++ src/App.vue
@@ -21,13 +21,21 @@
           <el-col :span="24" class="hidden-xs-only">
             <el-row>
               <el-col :span="5">
-                log
+                <div>
+                  <h1>
+                    <a href="#" title="logo">logo</a>
+                  </h1>
+                </div>
               </el-col>
               <el-col :span="15">
-                daohang
+                <el-tabs v-model="activeName" @tab-click="handleClick">
+                  <el-tab-pane label="主页" name="zhuye">主页</el-tab-pane>
+                  <el-tab-pane label="关于我们" name="guanyuwomen" >关于我们</el-tab-pane>
+                  <el-tab-pane label="联系我们" name="lianxiwomen">联系我们</el-tab-pane>
+                </el-tabs>
               </el-col>
               <el-col :span="4">
-                <el-input placeholder="请选择日期" suffix-icon="el-icon-date" v-model="input1"></el-input>
+                <el-input placeholder="输入关键字" suffix-icon="el-icon-search" v-model="input1"></el-input>
               </el-col>
             </el-row>
           </el-col>
@@ -38,23 +46,61 @@
 
     <el-main>
       <div class="block">
-        <el-carousel trigger="click" height="550px">
+        <el-carousel trigger="click" height="550px" indicator-position="none">
           <el-carousel-item v-for="item in 4" :key="item">
             <h3 class="small">{{ item }}</h3>
           </el-carousel-item>
         </el-carousel>
       </div>
+
+
     </el-main>
 
-    <el-footer>Footer</el-footer>
+    
+
+
+    <el-footer style="height: 400px;">
+      <el-row>
+        <el-col :span="24">
+            <div class="dingbu-1">
+                <el-row type="flex">
+                  <el-col :xs="22" :sm="22" :md="22" :lg="4" :xl="4">
+                    <div class="my-weibo">
+                      <el-button plain >关注我的微博</el-button>
+                    </div>
+                  </el-col>
+                </el-row>
+            </div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="20">
+            <div class="dingbu-2 top">
+               
+            </div>
+        </el-col>
+      </el-row>
+    </el-footer>
+
+
+
+
+
+
   </el-container>
 </template>
 
 <script>
 export default {
-  data(){
-    return{ 
-      input1:''
+  data() {
+    return {
+      input1: "",
+      activeName: "zhuye"
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
     }
   }
 };
@@ -103,11 +149,38 @@ export default {
 }
 
 .el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+  background-color: #163258;
 }
 
 .el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+  background-color: #1c8025;
+}
+
+.el-footer {
+  background-color: #eee;
+    padding: 0;
+    .dingbu-1{
+      height: 100px;
+      padding-top: 32px;
+      background-color: #bbb;
+      .el-row{
+        justify-content: center;
+        
+      }
+    }
+    .dingbu-2{
+      height: 60px;
+      background-color: #ccc;
+    }
+}
+
+.my-weibo{
+  background-color: #693a94;
+  height: 35px;
+  .el-button{
+    width: 100%;
+    border-radius: 0px
+  }
 }
 </style>
 
